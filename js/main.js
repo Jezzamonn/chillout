7eb23fafeb4d38ae7df88e79c1dde5872ecd8134
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-02-28 20:08:30 -0800

    Make it a circle

--- js/main.js
+++ js/main.js
@@ -32,21 +32,29 @@ function render() {
 	context.strokeStyle = 'blue';
 	context.lineWidth = 2;
 	for (let y = 0.5 * lineGap; y < canvas.height + lineGap; y += lineGap) {
-		context.moveTo(0, y)
-		for (let x = 0; x < canvas.width + 30; x += 1) {
+		let yAmt = 2 * y / canvas.height;
+		let gap = 1 - Math.sqrt(1 - (yAmt - 1) * (yAmt - 1));
+		let started = false;
+		for (let x = 0.5 * gap * canvas.width; x < (1 - 0.5 * gap) * canvas.width; x ++) {
 			let sineVal = (x / 5 + ms / 100);
 			let sineAmt = Math.sin((x - y) / 300 + ms / 500);
 			sineAmt = 0.5 + 0.5 * sineAmt;
-			sineAmt = easeInOut(sineAmt);
-			context.lineTo(x, y + 0.4 * lineGap * sineAmt * Math.sin(sineVal));
+			sineAmt = easeInOut(sineAmt, 1.5);
+			if (started) {
+				context.lineTo(x, y + 0.4 * lineGap * sineAmt * Math.sin(sineVal));
+			}
+			else {
+				context.moveTo(x, y + 0.4 * lineGap * sineAmt * Math.sin(sineVal));
+				started = true;
+			}
 		}
 	}
 	context.stroke();
 }
 
-function easeInOut(t) {
-	let tSq = t * t;
-	return tSq / (2 * tSq - 2 * t + 1);
+function easeInOut(t, amt) {
+	let tPow = Math.pow(t, amt);
+	return tPow / (tPow + Math.pow(1 - t, amt));
 }
 
 function handleResize(evt) {
